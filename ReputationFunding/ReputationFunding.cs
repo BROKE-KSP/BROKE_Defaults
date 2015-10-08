fc61a2864542deafeeaa2f2d6fa9a0d784c7acba
Ryan Pershing <ryan.pershing@gmail.com>
2015-10-07 19:08:17 -0700

    Added FM for government funding KSC, with settings and reputation based funding.

--- ReputationFunding/ReputationFunding.cs
+++ ReputationFunding/ReputationFunding.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using KSP;
+using UnityEngine;
 using BROKE;
 
 namespace BROKE_RepuationFunding
 {
     public class ReputationFunding : IFundingModifier
     {
+        float BASE_REP_FUNDING_YEARLY = 990.0f;
+        float BASE_FUNDING_YEARLY = 10000.0f;
 
         public string GetName()
         {
@@ -32,22 +35,30 @@ namespace BROKE_RepuationFunding
 
         public bool hasMainGUI()
         {
-            return false;
+            return true;
         }
 
         public void DrawMainGUI()
         {
-
+            GUILayout.Label("Hello, World!");
         }
 
         public bool hasSettingsGUI()
         {
-            return false;
+            return true;
         }
 
         public void DrawSettingsGUI()
         {
-
+            GUILayout.Label("Funding Settings:");
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Base Yearly Funding: ");
+            BASE_FUNDING_YEARLY = Single.Parse(GUILayout.TextField(BASE_FUNDING_YEARLY.ToString(), 10));
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Base Reputation Funding (per point/year): ");
+            BASE_REP_FUNDING_YEARLY = Single.Parse(GUILayout.TextField(BASE_REP_FUNDING_YEARLY.ToString(), 10));
+            GUILayout.EndHorizontal();
         }
 
         public void DailyUpdate()
@@ -57,12 +68,16 @@ namespace BROKE_RepuationFunding
 
         public InvoiceItem ProcessQuarterly()
         {
-            return null;
+            float f = Math.Max((int)Math.Ceiling((BASE_FUNDING_YEARLY + (BASE_REP_FUNDING_YEARLY * Reputation.CurrentRep))), 0);
+            var invoice = new InvoiceItem(this, f, 0);
+            return invoice;
         }
 
         public InvoiceItem ProcessYearly()
         {
-            return null;
+            float f = Math.Max((int)Math.Ceiling(((BASE_FUNDING_YEARLY / 4) + ((BASE_REP_FUNDING_YEARLY / 4) * Reputation.CurrentRep))), 0);
+            var invoice = new InvoiceItem(this, f, 0);
+            return invoice;
         }
 
         public ConfigNode SaveData()
